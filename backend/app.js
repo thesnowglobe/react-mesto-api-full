ec42c46922301480fea0ceb1c198656988ec8641
Alena M <aly0mal99@gmail.com>
2023-01-20 15:11:34 +0100

    localhost change + mongoose

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.post('/signin', signInValidation, login);
 app.post('/signup', signUpValidation, createUser);
