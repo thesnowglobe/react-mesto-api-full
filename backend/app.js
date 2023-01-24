d5590184ec2c6cc930daae11d7c86b7e7a621ced
Alena M <aly0mal99@gmail.com>
2023-01-24 18:44:50 +0100

    crash test

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ app.use(helmet());
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', signInValidation, login);
 app.post('/signup', signUpValidation, createUser);
 
